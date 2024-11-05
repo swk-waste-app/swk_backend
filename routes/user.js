0cd8b63cb16b7a24dbc565265491b068b5cb15b9
Frank Koomson <frankkoomson1@gmail.com>
2024-11-05 07:37:25 +0000

    Updated

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { registerUser, loginUser, getProfile, updateProfile, getUserProducts } from '../controllers/user.js';
+import { userProfileImageUpload } from '../middlewares/uploads.js';
 import { isAuthenticated, hasPermission } from '../middlewares/auth.js';
 
 const userRouter = Router();
@@ -14,10 +15,10 @@ userRouter.post('/users/login', loginUser);
 // Route to get the logged-in user's profile (must be authenticated)
 userRouter.get('/users/me', isAuthenticated, getProfile);
 
-userRouter.get('/users/me/adverts', isAuthenticated, getUserAdverts);
+userRouter.get('/users/me/adverts', isAuthenticated, getUserProducts);
 
 // Route to update the user's profile (must be authenticated)
-userRouter.patch('/users/me', isAuthenticated, hasPermission('update_profile'), userAvatarUpload.single('avatar'), updateProfile);
+userRouter.patch('/users/me', isAuthenticated, hasPermission('update_profile'), userProfileImageUpload.single('avatar'), updateProfile);
 
 // Export router
 export default userRouter;
