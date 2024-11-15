1e91943d4696d3bc35095512caa1ea7c99d91a06
Frank Koomson <frankkoomson1@gmail.com>
2024-11-15 11:09:49 +0000

    new update

--- routes/wasteCollection.js
+++ routes/wasteCollection.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { schedulePickup, getPickupHistory, updatePickupStatus } from '../controllers/wasteCollection.js';
+import { schedulePickup, getPickupHistory, updatePickupStatus, getSchedule } from '../controllers/wasteCollection.js';
 import { isAuthenticated } from '../middlewares/auth.js'; 
 
 const wasteCollectionRouter = Router();
 
 wasteCollectionRouter.post('/schedule', isAuthenticated, schedulePickup);
 wasteCollectionRouter.get('/history', isAuthenticated, getPickupHistory);
-wasteCollectionRouter.get('/schedule/:id', getProduct)
+wasteCollectionRouter.get('/schedule/:id', isAuthenticated, getSchedule);
 wasteCollectionRouter.patch('/:id/status', isAuthenticated, updatePickupStatus);
 
 export default wasteCollectionRouter; 
