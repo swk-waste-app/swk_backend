cac66b2c063a5d8816389b7fa71bf96eac2672d7
Frank Koomson <frankkoomson1@gmail.com>
2026-03-21 23:53:59 +0000

    Validator user

--- validators/user.js
+++ validators/user.js
@@ -1,25 +1,19 @@
 import Joi from 'joi';
 
-// Validator for user registration
 export const registerUserValidator = Joi.object({
-    name: Joi.string().required(),            
-    email: Joi.string().email().required(),    
-    password: Joi.string().min(6).required(), 
-    contactNumber: Joi.number(),
-    businessName: Joi.string(),
-    role: Joi.string().valid('user', 'vendor', 'admin').required()
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    location: Joi.string(),
+    role: Joi.string().valid('user', 'vendor', 'admin').default('user') // no longer required
 });
 
-// Validator for user login
 export const loginUserValidator = Joi.object({
-    email: Joi.string().email().required(),    
-    password: Joi.string().required()         
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
 });
 
-// Validator for updating user profile
 export const updateProfileValidator = Joi.object({
-    name: Joi.string(),                       
-    avatar: Joi.string()                       
+    name: Joi.string(),
+    avatar: Joi.string()
 });
-
-
