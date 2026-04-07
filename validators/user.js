f4cbd775c04ba313a09eea60d89378efaf74832b
Frank Koomson <frankkoomson1@gmail.com>
2026-04-07 01:21:19 +0000

    Fix MongoDB connection string and update validator to accept contactNumber

--- validators/user.js
+++ validators/user.js
@@ -4,8 +4,8 @@ export const registerUserValidator = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
-    location: Joi.string(),
-    role: Joi.string().valid('user', 'vendor', 'admin').default('user') // no longer required
+    contactNumber: Joi.number(),
+    role: Joi.string().valid('user', 'vendor', 'admin').default('user')
 });
 
 export const loginUserValidator = Joi.object({
